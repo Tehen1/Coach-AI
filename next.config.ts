04423acf53e3dc4b8652aba5ee8ddae6be89f82f
Cyborg Punk <dwtehen1@gmail.com>
2025-08-25 09:30:21 +0000

    6000-firebase-studio-1755495189420.cluster-cbeiita7rbe7iuwhvjs5zww2i4.cl

--- next.config.ts
+++ next.config.ts
@@ -1,43 +1,6 @@
 
 import type {NextConfig} from 'next';
 
-const cspHeader = `
-    default-src 'self';
-    script-src 'self' 'unsafe-eval' 'unsafe-inline';
-    style-src 'self' 'unsafe-inline';
-    img-src 'self' blob: data: https://placehold.co;
-    font-src 'self';
-    object-src 'none';
-    base-uri 'self';
-    form-action 'self';
-    block-all-mixed-content;
-    upgrade-insecure-requests;
-`;
-
-const securityHeaders = [
-  {
-    key: 'Content-Security-Policy',
-    value: cspHeader.replace(/\s{2,}/g, ' ').trim()
-  },
-  {
-    key: 'X-Content-Type-Options',
-    value: 'nosniff',
-  },
-  {
-    key: 'X-XSS-Protection',
-    value: '1; mode=block',
-  },
-   {
-    key: 'Referrer-Policy',
-    value: 'origin-when-cross-origin'
-  },
-  {
-    key: 'Permissions-Policy',
-    value: "camera=(), microphone=(), geolocation=()"
-  }
-];
-
-
 const nextConfig: NextConfig = {
   /* config options here */
   typescript: {
@@ -58,15 +21,8 @@ const nextConfig: NextConfig = {
   },
   poweredByHeader: false,
   productionBrowserSourceMaps: false,
-  async headers() {
-    return [
-      {
-        // Apply these headers to all routes in your application.
-        source: '/(.*)',
-        headers: securityHeaders,
-      },
-    ]
-  },
 };
 
 export default nextConfig;
+
+    
